ac7f8ba4bd0174059f08ab5635ef1d853a5b499f
salymantimmy <salymantimmy@gmail.com>
2022-10-21 09:38:01 -0700

    My second commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ * @n:  number of times the character
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('\\');
+}
+_putchar('\n');
+}
